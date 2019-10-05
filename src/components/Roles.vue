5f34de0d4c1beb8f0efb75aa894b03b9d3ce5674
jing <719666858@qq.com>
2019-10-05 23:54:26 +0800

    角色列表的深度渲染

--- src/components/Roles.vue
+++ src/components/Roles.vue
@@ -12,13 +12,40 @@
 
     <!-- 表格 -->
     <el-table :data="rolesList" style="width: 100%">
+      <el-table-column type="expand">
+        <template v-slot:default="{ row }">
+          <el-row v-for="level1 in row.children" :key="level1.id" class="level1">
+            <el-col :span="4">
+              <el-tag closable @close="delRights(row, level1.id)">{{ level1.authName }}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row v-for="level2 in level1.children" :key="level2.id" class="level2">
+                <el-col :span="4">
+                  <el-tag type="success" closable @close="delRights(row, level2.id)">{{ level2.authName }}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    v-for="level3 in level2.children"
+                    :key="level3.id"
+                    type="warning"
+                    class="level3"
+                    closable
+                     @close="delRights(row, level3.id)"
+                  >{{ level3.authName }}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column type="index"></el-table-column>
       <el-table-column prop="roleName" label="角色名称"></el-table-column>
       <el-table-column prop="roleDesc" label="描述"></el-table-column>
 
       <el-table-column label="操作">
         <template v-slot:default="obj">
-
           <!-- 修改用户 -->
           <el-button
             @click="showEditDialog(obj.row)"
@@ -43,7 +70,7 @@
               <el-button @click="editDialogFormVisible = false">取 消</el-button>
               <el-button @click="editUser" type="primary">确 定</el-button>
             </div>
-          </el-dialog> -->
+          </el-dialog>-->
           <!-- 删除用户 -->
           <el-button
             plain
@@ -68,17 +95,49 @@ export default {
     }
   },
 
-  async created () {
-    const { data, meta } = await this.$axios.get('roles')
-    if (meta.status === 200) {
-      this.rolesList = data
-    } else {
-      this.$message.error(meta.msg)
+  created () {
+    this.getRoleList()
+  },
+
+  methods: {
+    // 角色列表
+    async getRoleList () {
+      const { data, meta } = await this.$axios.get('roles')
+      if (meta.status === 200) {
+        this.rolesList = data
+      } else {
+        this.$message.error(meta.msg)
+      }
+    },
+    // 删除角色的指定权限
+    async delRights (row, rightId) {
+      // console.log(row)
+      const { data, meta } = await this.$axios.delete(`roles/${row.id}/rights/${rightId}`)
+      if (meta.status === 200) {
+        this.$message.success(meta.msg)
+        row.children = data
+      } else {
+        this.$message.error(meta.msg)
+      }
     }
   }
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+.roles {
+  .el-table {
+    .level1 {
+      margin-bottom: 5px;
+      border-bottom: 1px dotted #ccc;
+    }
+    .level2 {
+      margin-bottom: 5px;
+    }
+    .level3 {
+      margin-right: 5px;
+      margin-bottom: 5px;
+    }
+  }
+}
 </style>
