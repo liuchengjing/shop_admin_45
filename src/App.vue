76ddc38d63951d4f2272ae1d6ce5675ac394cc0f
jing <719666858@qq.com>
2019-10-05 17:00:24 +0800

    权限列表展示完成

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang='scss'>
-  * {
+* {
   margin: 0;
   padding: 0;
 }
-html, body, #app {
+html,
+body,
+#app {
   width: 100%;
   height: 100%;
+}
+
+#app {
+  .el-breadcrumb {
+    height: 40px;
+    line-height: 40px;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 10px;
+  }
 }
 </style>
--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,10 +1,45 @@
 <template>
-  <div>这是rights组件</div>
+  <div class="rights">
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 表格 -->
+  <el-table :data="rightsData" style="width: 100%">
+    <el-table-column type="index"></el-table-column>
+    <el-table-column prop="authName" label="权限名称"></el-table-column>
+    <el-table-column prop="path" label="路径" ></el-table-column>
+    <el-table-column label="层级">
+      <template v-slot:default="{ row }">
+        <p v-if="row.level === '0'">一级</p>
+        <p v-if="row.level === '1'">二级</p>
+        <p v-if="row.level === '2'">三级</p>
+      </template>
+    </el-table-column>
+  </el-table>
+  </div>
+
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      rightsData: []
+    }
+  },
 
+  async created () {
+    const { meta, data } = await this.$axios.get('rights/list')
+    if (meta.status === 200) {
+      this.rightsData = data
+    } else {
+      this.$message.error(meta.msg)
+    }
+  }
 }
 </script>
 
