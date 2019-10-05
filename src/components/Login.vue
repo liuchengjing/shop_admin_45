28e01dd680ce088ee85355d057ef91fea0bb9a6f
jing <719666858@qq.com>
2019-10-05 15:29:53 +0800

    补充：回车登录，即给组件注册原生事件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,10 +3,10 @@
     <el-form ref="form" :model="form" label-width="80px" :rules="rules" status-icon>
       <img src="../assets/avatar.jpg" alt />
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+        <el-input @keyup.enter.native="login" v-model="form.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
+        <el-input @keyup.enter.native="login" v-model="form.password" placeholder="请输入密码" type="password"></el-input>
       </el-form-item>
       <el-button type="primary" class="login-btn" @click="login">登录</el-button>
       <el-button @click="reset">重置</el-button>
@@ -42,55 +42,6 @@ export default {
       // this.$ref.form.resetFields()  //$refs少写了一个s
       this.$refs.form.resetFields()
     },
-    //   login () {
-    //     this.$refs.form.validate(isValid => {
-    //       if (!isValid) return
-    //       // console.log('发送ajax请求')
-    //       // axios({
-    //       //   method: 'post',
-    //       //   url: 'http://localhost:8888/api/private/v1/login',
-    //       //   data: this.form
-    //       // }).then(res => {
-    //       //   console.log(res.data)
-    //       //   if (res.data.meta.status === 200) {
-    //       //     // 登录成功，跳转到首页
-    //       //     console.log(res.data.meta.msg)
-    //       //   } else {
-    //       //     // 给用户提示，登录失败
-    //       //     console.log(res.data.meta.msg)
-    //       //   }
-    //       // })
-
-    //       // ajax优化
-    //       this.$axios.post('login', this.form)
-    //         .then(res => {
-    //           const { meta } = res
-    //           const { data } = res
-    //           if (meta.status === 200) {
-    //             // 登录成功，跳转到首页
-    //             // console.log(meta.msg)
-    //             // 消息提示框
-    //             this.$message({
-    //               message: '登录成功',
-    //               type: 'success',
-    //               duration: 1000
-    //             })
-    //             // 跳转到首页
-    //             this.$router.push({ name: 'index' })
-    //             localStorage.setItem('token', data.token)
-    //           } else {
-    //             // 给用户提示，登录失败
-    //             // console.log(meta.msg)
-    //             this.$message({
-    //               message: meta.msg,
-    //               type: 'error',
-    //               duration: 1000
-    //             })
-    //           }
-    //         })
-    //     })
-    //   }
-    // }
 
     // login的async await优化
     async login () {
